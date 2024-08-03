f744a793a252bd13322f3e4105a3fabf98bb693f
Jose Quesada <jose.quesada@fernando3martos.com>
2024-08-03 08:49:48 +0200

    Petición HTTP

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
+
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif [] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey:       string = '9sRmUi7HCk50jblcNpzwSn4IE5hyi04t';
+  private serviceUrl:   string = 'Http://api.giphy.com/v1/gifs';
+
+  constructor( private http:HttpClient) { }
 
   get tagHistory (){
     return [...this._tagsHistory];
@@ -28,6 +35,17 @@ export class GifsService {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+      .set ('api_key', this.apiKey)
+      .set ('limit', '10')
+      .set ('q', tag)
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
+      .subscribe ( resp => {
+        this.gifList = resp.data;
+        console.log({gif: this.gifList});
+        }
+     )
   }
 
 }
